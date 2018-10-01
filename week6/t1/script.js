6ccc98da9a2fe916294c8549ee98925c0922e856
adilbeq <38554034+adilbeq@users.noreply.github.com>
2018-10-02 02:30:00 +0600

    new

---
+++ week6/t1/script.js
@@ -0,0 +1,19 @@
+function fWeb () {
+	web.setAttribute("data-status", "done");
+}
+
+function fGym () {
+	gym.setAttribute("data-status", "done");
+}
+
+function fHome () {
+	home.setAttribute("data-status", "done");
+}
+
+function fProj () {
+	proj.setAttribute("data-status", "done");
+}
+
+function fCalc () {
+	calc.setAttribute("data-status", "done");
+}
